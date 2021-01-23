8880884f13ad4c43454ead6e08d94050ab71d8aa
Benjamin Fiske <bffiske@gmail.com>
2021-01-23 12:46:02 -0500

    Update App.js

--- client/App.js
+++ client/App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import CompTest from './CompTest';
+import CompTest from './Components/CompTest';
 
 export default function App() {
   return (
