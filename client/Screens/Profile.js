971141c0504062b5fd037e07d6781cede0dc6362
naafiyan <naafiyan@gmail.com>
2021-01-24 18:31:37 +0600

    profile page

--- client/Screens/Profile.js
+++ client/Screens/Profile.js
@@ -1,50 +1,90 @@
 import React, { useContext } from "react";
-import { ScrollView, StyleSheet, Text, View, Dimensions } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  FlatList,
+} from "react-native";
 import { UserContext } from "../Contexts/UserContext";
 import NavBar from "../Components/NavBar";
+import { Avatar, Card } from "react-native-elements";
+
+const treeImg = require("../Images/tree.jpeg");
+
+const userDummyData = {
+  totalDonation: 65,
+  achievements: ["Planted 9 trees", "Picked up 12 pounds of ocean plastic"],
+};
 
 const screenWidth = Dimensions.get("window").width;
 export default function Profile({ navigation }) {
   const userCtx = useContext(UserContext);
 
+  const renderItem = ({ item }) => {
+    return (
+      <Card>
+        <Text>{item}</Text>
+      </Card>
+    );
+  };
+
   return (
     <>
-      <View style={styles.blueBg}>
-        <Text style={styles.profileTxt}>Your Profile</Text>
-      </View>
-      <View style="userHeader">
-        <Text style={styles.username}>{userCtx.user}</Text>
-      </View>
+      <View style={styles.container}>
+        <View style={styles.blueBg}>
+          <Text style={styles.profileTxt}>Your Profile</Text>
+          <View style={styles.profilePic}>
+            <Avatar
+              rounded
+              size="xlarge"
+              source={{ uri: treeImg.toString() }}
+              activeOpacity={0.7}
+            />
+          </View>
+        </View>
+        <View style={styles.userHeader}>
+          <Text style={styles.username}>{userCtx.user}</Text>
+          <Text style={{ fontSize: 20, marginTop: 20, marginBottom: 10 }}>
+            Total Donations: {userDummyData.totalDonation}
+          </Text>
+          <Text style={{ fontSize: 20 }}>Achievements</Text>
+        </View>
+        <View style={styles.achievementsList}>
+          <FlatList data={userDummyData.achievements} renderItem={renderItem} />
+        </View>
 
-      <NavBar navigation={navigation} />
+        <NavBar navigation={navigation} />
+      </View>
     </>
   );
 }
 
 const styles = StyleSheet.create({
   blueBg: {
-    position: "absolute",
-    height: 245,
-    left: 0,
-    right: -1,
-    top: 0,
     backgroundColor: "#56CCF2",
+    alignItems: "center",
   },
-  username: {
-    position: "absolute",
-    width: 357,
-    height: 125,
-    left: screenWidth / 2 - 50,
-    top: 289,
-  },
+  username: { fontSize: 40 },
   userHeader: {
     flex: 1,
+    alignItems: "center",
+    marginTop: 80,
   },
-  profileTxt: {
-    position: "absolute",
-    width: 94,
-    height: 36,
-    left: screenWidth / 2 - 50,
-    top: 20,
+  profileTxt: { fontSize: 30, color: "white", marginVertical: 20 },
+  container: {
+    flex: 1,
+    alignItems: "stretch",
+    backgroundColor: "white",
+  },
+  profilePic: {
+    marginBottom: -60,
+  },
+  achievementsList: {
+    flex: 1,
+    marginTop: -350,
+    justifyContent: "flex-start",
+    alignItems: "stretch",
   },
 });
